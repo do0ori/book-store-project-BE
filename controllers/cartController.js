0a79fa3db9f0dd378c0091bd0415c0ecca78f3a9
do0ori <fuzzydo0ori@gmail.com>
2024-01-08 16:42:17 +0900

    feat: Implement cart API

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -2,24 +2,80 @@ const conn = require('../mariadb');
 const { StatusCodes } = require('http-status-codes');
 
 const addToCart = (req, res) => {
-    res.json("장바구니 담기");
+    const { userId, bookId, quantity } = req.body;
+
+    const sql = "INSERT INTO cart (user_id, book_id, quantity) VALUES (?, ?, ?)";
+    const values = [userId, bookId, quantity];
+    conn.query(
+        sql, values,
+        (err, results) => {
+            if (err) {
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).end();
+            }
+
+            return res.status(StatusCodes.CREATED).json(results);
+        }
+    );
 };
 
-const getCart = (req, res) => {
-    res.json("장바구니 조회");
+const getCartItems = (req, res) => {
+    const { userId, selected } = req.body;
+
+    let sql = `
+        SELECT
+            item_id,
+            id AS book_id,
+            title,
+            summary,
+            quantity,
+            price
+        FROM cart
+        LEFT JOIN books
+        ON cart.book_id = books.id
+        WHERE user_id = ?
+    `;
+    const values = [userId];
+    if (selected) {
+        sql += "AND item_id IN (?)";
+        values.push(selected);
+    }
+    conn.query(
+        sql, values,
+        (err, results) => {
+            if (err) {
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).end();
+            }
+
+            return res.status(StatusCodes.OK).json(results);
+        }
+    )
 };
 
 const removeFromCart = (req, res) => {
-    res.json("장바구니 삭제");
-};
+    const { itemId } = req.params;
 
-const getSelectedItemsFromCart = (req, res) => {
-    res.json("장바구니에서 선택한 '주문 예상' 상품 목록 조회");
+    const sql = "DELETE FROM cart WHERE item_id = ?";
+    conn.query(
+        sql, itemId,
+        (err, results) => {
+            if (err) {
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).end();
+            }
+
+            if (results.affectedRows == 0) {
+                return res.status(StatusCodes.BAD_REQUEST).end();
+            } else {
+                return res.status(StatusCodes.OK).json(results);
+            }
+        }
+    );
 };
 
 module.exports = {
     addToCart,
-    getCart,
-    removeFromCart,
-    getSelectedItemsFromCart
+    getCartItems,
+    removeFromCart
 };
--- routes/carts.js
+++ routes/carts.js
@@ -2,14 +2,12 @@ const express = require('express');
 const router = express.Router();
 const {
     addToCart,
-    getCart,
-    removeFromCart,
-    getSelectedItemsFromCart
+    getCartItems,
+    removeFromCart
 } = require('../controllers/cartController');
 
 router.post('/', addToCart);
-router.get('/', getCart);
-router.delete('/:bookId', removeFromCart);  // bookId가 아니라 cart의 itemId가 될 수도
-router.post('/items', getSelectedItemsFromCart);    // uri 미확정. 일단 임시로.
+router.get('/', getCartItems);
+router.delete('/:itemId', removeFromCart);
 
 module.exports = router;
