10c96e67589a7131d92adcc16eb51a98bf7e3326
do0ori <fuzzydo0ori@gmail.com>
2024-01-16 00:45:25 +0900

    feat: Add case for foreign key constraint violation in errorHandler

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -13,13 +13,17 @@ const errorHandler = (err, req, res, next) => {
     if (req.connection) {
         req.connection.release();
     }
+
+    console.error(`>> ${new Date().toLocaleString()}: ${err.stack}`);
+
     if (err instanceof HttpError) {
         res.status(err.statusCode).json({ message: err.message });
     } else if (err instanceof TokenExpiredError || err instanceof JsonWebTokenError || err instanceof NotBeforeError) {
         res.status(StatusCodes.UNAUTHORIZED).send({ message: err.message });
+    } else if (["ER_NO_REFERENCED_ROW", "ER_ROW_IS_REFERENCED", "ER_ROW_IS_REFERENCED_2", "ER_NO_REFERENCED_ROW_2"].includes(err.code)) {
+        res.status(StatusCodes.BAD_REQUEST).send({ message: "A foreign key constraint fails. Send a valid input." })
     } else {
-        console.error(`>> ${new Date().toLocaleString()}\n${err.stack}`);
-        res.status(StatusCodes.BAD_REQUEST).send({ message: "Something went wrong!" });
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({ message: "Something went wrong!" });
     }
 };
 
