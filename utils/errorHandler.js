bffa6ac736d7e078c4d801b3a21d62858b7ffe8a
do0ori <fuzzydo0ori@gmail.com>
2024-01-16 14:55:10 +0900

    refactor: Simply check whole sql driven errors with error code prefix "ER_"

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -20,8 +20,8 @@ const errorHandler = (err, req, res, next) => {
         res.status(err.statusCode).json({ message: err.message });
     } else if (err instanceof TokenExpiredError || err instanceof JsonWebTokenError || err instanceof NotBeforeError) {
         res.status(StatusCodes.UNAUTHORIZED).send({ message: err.message });
-    } else if (["ER_NO_REFERENCED_ROW", "ER_ROW_IS_REFERENCED", "ER_ROW_IS_REFERENCED_2", "ER_NO_REFERENCED_ROW_2"].includes(err.code)) {
-        res.status(StatusCodes.BAD_REQUEST).send({ message: "A foreign key constraint fails. Send a valid input." })
+    } else if (err.code.startsWith("ER_")) {
+        res.status(StatusCodes.BAD_REQUEST).send({ message: "Send valid inputs." })
     } else {
         res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({ message: "Something went wrong!" });
     }
